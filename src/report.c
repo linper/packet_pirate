4f737bce1469ef88b4c0c6bc42273af68b50cf14
linper <llinas7879@gmail.com>
2022-02-16 11:25:59 +0200

    added tree structure vizualizatioon to report

--- src/report.c
+++ src/report.c
@@ -1,31 +1,50 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #include "../include/report.h"
 #include "../include/ext_filter.h"
 #include "../include/ef_tree.h"
 
+static void build_prefix(struct ef_tree *node, void *usr)
+{
+	if (!node->par) {
+		return;
+	}
+
+	size_t len = strlen((char *)usr);
+	char *wrt_ptr = ((char *)usr) + len;
+	sprintf(wrt_ptr, "%s", node->next ? "│ " : "  ");
+}
+
 static void report_one(struct ef_tree *node, void *usr)
 {
 	(void)usr;
 
+	char dent[64] = "";
+
+	ef_tree_root_to_leaf_foreach(pc.ef_root, node->par, build_prefix, dent);
+
+	const char *interc = node->next ? "├─" : "└─";
+	const char *next_br = node->next ? "│ " : "  ";
+	const char *chld_br = node->chld ? "│ " : "  ";
+
 	struct filter *f = node->flt->filter;
 	struct report *r = &node->flt->rep;
 
-	printf("%s:\n", f->packet_tag);
-	printf("\treceived: %ld\n", r->received);
-	printf("\tskiped: %ld\n", r->skiped);
-	printf("\tunconverted: %ld\n", r->unconverted);
-	printf("\tunsplit: %ld\n", r->unsplit);
-	printf("\tinvadidated: %ld\n", r->invalid);
-	printf("\ttruncated: %ld\n", r->truncated);
-	printf("\tparsed: %ld\n", r->parsed);
-	printf("\n");
+	printf("%s%s%s:\n", dent, interc, f->packet_tag);
+	printf("%s%s%sreceived: %ld\n", dent, next_br, chld_br, r->received);
+	printf("%s%s%sskiped: %ld\n", dent, next_br, chld_br, r->skiped);
+	printf("%s%s%sunconverted: %ld\n", dent, next_br, chld_br, r->unconverted);
+	printf("%s%s%sunsplit: %ld\n", dent, next_br, chld_br, r->unsplit);
+	printf("%s%s%snvadidated: %ld\n", dent, next_br, chld_br, r->invalid);
+	printf("%s%s%struncated: %ld\n", dent, next_br, chld_br, r->truncated);
+	printf("%s%s%sparsed: %ld\n", dent, next_br, chld_br, r->parsed);
+	printf("%s%s%s\n", dent, next_br, chld_br);
 }
 
 void report_all()
 {
-	//TODO add some sort of tree structure visuolization
 	ef_tree_foreach(pc.ef_root, true, report_one, NULL);
 
 	//mabe add agregated report TODO
